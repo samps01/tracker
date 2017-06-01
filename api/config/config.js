e95727e26528c816074741b0d75e7ab80c94152e
samps01 <samps01@gmail.com>
2017-05-31 22:46:35 -0400

    Intitial API setup with 2 primary endpoints

---
+++ api/config/config.js
@@ -0,0 +1,7 @@
+const env = process.env.NODE_ENV || 'development';
+
+if(env === "development"){
+    process.env.PORT = 3000;
+}else {
+    process.env.PORT = 3000;
+}
---
+++ api/server/server.js
@@ -0,0 +1,32 @@
+//Requiring config setting for the app
+require('../config/config');
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods","OPTIONS,PUT,POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.put('/vehicles',(req,res)=>{
+    console.log(req.body.length);
+    res.send();
+});
+
+app.post('/readings',(req,res)=>{
+   console.log(req.body);
+   res.send();
+});
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`);
+});
